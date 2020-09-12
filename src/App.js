d6276c6f3d9141e2a1ad592286bd69a2165bd500
Felertooshia <67173720+Felertooshia@users.noreply.github.com>
2020-09-12 12:26:07 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
-import TodoItem from "./components/TodoItem";
-import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem/TodoItem";
+import TodoForm from "./components/TodoForm/TodoForm";
+import "./App.css";
 
 function App() {
   //variable
   const [todos, setTodos] = useState([
-    "Finish plus project",
-    "Fedd peggy",
-    "Sleep",
+    { text: "Finish plus project", isCompleted: false },
+    { text: "Fedd peggy", isCompleted: false },
+    { text: "Sleep", isCompleted: false },
   ]);
 
   //methods
   const addTodo = (text) => {
-    const newTodos = [...todos, text];
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
     setTodos(newTodos);
   };
 
   //template
   return (
-    <div>
-    <h1>My todo list</h1>
-    {todos.map((todo, index) => (
-      <TodoItem todo={todo} key={index} />
-    ))}
-    <TodoForm addTodo={addTodo} />
-    </div>
+    <div className="app">
+      <div className="todo-list">
+        <h1>My todo list</h1>
+        {todos.map((todo, index) => (
+          <TodoItem
+            todo={todo}
+            key={index}
+            index={index}
+            completeTodo={completeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>    
   );
 }
 
